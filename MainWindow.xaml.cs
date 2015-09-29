f18b764a6c059ecdeb704609ec10fbebb0913812
AbelGarace <abel.garace@gmail.com>
2015-09-29 13:28:13 +0200

    First Commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using ControlTemplate.Data;
+using ControlTemplate.ViewModel;
+using System.Windows;
+
+namespace ControlTemplate
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ListBoxViewModel listDependency;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            lbTodoList.DataContext = new ListBoxViewModel();
+            listDependency = (ListBoxViewModel)lbTodoList.DataContext;
+
+            User user1 = new User() { Name = "User1" };
+            User user2 = new User() { Name = "User2" };
+            User user3 = new User() { Name = "User3" };
+            User user4 = new User() { Name = "User4" };
+            User user5 = new User() { Name = "User5" };
+
+            listDependency.List.Add(user1);
+            listDependency.List.Add(user2);
+            listDependency.List.Add(user3);
+            listDependency.List.Add(user4);
+            listDependency.List.Add(user5);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            User user = new User() { Name = "New User" };
+            listDependency.List.Add(user);
+        }
+    }
+}
---
+++ ViewModel/ListBoxViewModel.cs
@@ -0,0 +1,19 @@
+﻿using ControlTemplate.Data;
+using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace ControlTemplate.ViewModel
+{
+    public class ListBoxViewModel :  DependencyObject
+    {
+        public ObservableCollection<User> List
+        {
+            get { return (ObservableCollection<User>)GetValue(ListProperty); }
+            set { SetValue(ListProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for List.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ListProperty =
+            DependencyProperty.Register("List", typeof(ObservableCollection<User>), typeof(ListBoxViewModel), new PropertyMetadata(new ObservableCollection<User>()));
+    }
+}
